425eff0c17356d2f53ae1998fb03fe29fc1ad73c
zakcrust <zakcrusted@gmail.com>
2020-07-13 19:30:28 +0700

    implemen dynamic patrol-points (A-B)

--- Scripts/Bandit/Bandit.gd
+++ Scripts/Bandit/Bandit.gd
@@ -27,13 +27,17 @@ var npc_actions  : Dictionary = {
 }
 
 enum {
+	idle,
 	move,
 	attack_1,
-	dead
+	dead,
+	patrol,
+	follow
 }
 
 var state : int = move
-
+var sub_state : int
+var temp_state : int
 
 
 
@@ -55,6 +59,7 @@ func _handle_input(_delta):
 		move:
 			if Input.is_action_pressed("npc_attack") and is_on_floor():
 					$AnimSprite.play("attack")
+					temp_state = state
 					state = attack_1
 					$AttackTimer.start()
 					$HitTimer.start()
@@ -80,6 +85,11 @@ func _handle_input(_delta):
 				$AnimSprite.play("jump")
 				velocity.y = -JUMP_SPEED
 			
+			
+			match sub_state:
+				patrol:
+					travel_distance(_delta)
+	
 	
 	velocity.y += (GRAVITY - slide_modifier)
 	velocity = move_and_slide(velocity, Vector2.UP)
@@ -95,26 +105,32 @@ func check_sword_hit() -> void:
 
 
 func _on_AttackTimer_timeout():
-	state = move
+	state = temp_state
 
 
 func _patrol() -> void:
+	sub_state = patrol
+	state = move
 	patrol_points.shuffle()
 	var choosen_point = patrol_points.front()
 	var debug_marker = marker_debug.instance()
+	var distance_to = global_position.distance_to(Vector2(choosen_point.x, global_position.y))
+	while(distance_to < 5.0):
+		patrol_points.shuffle()
+		choosen_point = patrol_points.front()
+		distance_to = global_position.distance_to(Vector2(choosen_point.x, global_position.y))
+	patrol_distance = distance_to
 	debug_marker.position = Vector2(choosen_point.x, global_position.y)
 	get_parent().call_deferred("add_child", debug_marker)
-	var distance_to = global_position.distance_to(Vector2(choosen_point.x, global_position.y))
 	var patrol_time = distance_to / SPEED
 	global_position.linear_interpolate(Vector2(choosen_point.x, global_position.y), patrol_time)
+	print("DISTANCE : %s" % distance_to)
 	print("INTERPOLATION TIME : %s" % (patrol_time))
-	$PatrolTime.set_wait_time(patrol_time)
-	$PatrolTime.start()
-	print("PATROL TIME LEFT : %s " % $PatrolTime.time_left)
 	_check_patrol_anim(choosen_point)
 
 
 func _reset_state() -> void:
+	state = idle
 	Input.action_release("npc_move_left")
 	Input.action_release("npc_move_right")
 	Input.action_release("npc_jump")
@@ -129,6 +145,16 @@ func _check_patrol_anim(choosen_point) -> void:
 	else:
 		$AnimSprite.flip_h = false
 		Input.action_press("npc_move_right")
+
+
+func travel_distance(delta):
+	if patrol_distance > 0:
+		patrol_distance -= SPEED * delta
+		print(patrol_distance)
+	else:
+		_reset_state()
+		$IdleTimer.start()
+
 
 
 func _on_ScanBox_body_entered(body):
@@ -148,8 +174,3 @@ func _on_HitTimer_timeout():
 
 func _on_IdleTimer_timeout():
 	_patrol()
-
-
-func _on_PatrolTime_timeout():
-	_reset_state()
-	$IdleTimer.start()
